718060a59d64f6b2daccc827eaf46a9e4ac5de47
Sergey Romanyuk <sergey.romanyuk@dataart.com>
2019-01-27 11:56:00 +0200

    Add project files.

---
+++ ESP_32_WaveShare.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       ESP_32_WaveShare.ino
+    Created:	19/01/27 11:53:57 AM
+    Author:     UNIVERSE\sromanyuk
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
