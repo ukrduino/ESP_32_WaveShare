361af4d0ea5e7dc099b3be327967d0d3f7a0e466
Sergey Romanyuk <sergey.romanyuk@dataart.com>
2019-03-08 11:39:35 +0200

    Openweather map and forex working

--- ESP_32_WaveShare.ino
+++ ESP_32_WaveShare.ino
@@ -19,19 +19,21 @@ String APIKEY = "85caa036010dce285793ea9a1a494fea";
 //--------------------------------WIFI----------------------------------------------------
 WiFiClient espClient;
 
-//----------------------------------Currency---------------------------------------------
-//https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFiClientSecure/examples/WiFiClientSecure/WiFiClientSecure.ino
+//----------------------------------Forex---------------------------------------------
 //https://1forge.com/forex-data-api/api-documentation
+char* usd_to_pln = "http://forex.1forge.com/1.0.3/convert?from=USD&to=PLN&quantity=1&api_key=C0PpsLfvczArzUfXOZdK7pkrskTmRP0q";
+char* eur_to_usd = "http://forex.1forge.com/1.0.3/convert?from=USD&to=PLN&quantity=1&api_key=C0PpsLfvczArzUfXOZdK7pkrskTmRP0q";
 #include <HTTPClient.h>
+#include <TimeLib.h>
 HTTPClient http;
-#include <time.h>    // Built-in
+
 
 void setup()
 {
 	Serial.begin(115200);
 	setup_wifi();
 	getWeatherData();
-	//getCurrencyData();
+	getForexData();
 }
 
 void setup_wifi() {
@@ -55,13 +57,9 @@ void loop()
 
 }
 
-
-
-
-void getWeatherData() //client function to send/receive GET request data.
-{
-	String payload;
-	http.begin("http://forex.1forge.com/1.0.3/convert?from=EUR&to=USD&quantity=1&api_key=C0PpsLfvczArzUfXOZdK7pkrskTmRP0q");
+String getForexRate(char* requestString) {
+	String payload = "";
+	http.begin(requestString);
 	int httpCode = http.GET();
 	if (httpCode > 0) { //Check for the returning code
 
@@ -71,9 +69,13 @@ void getWeatherData() //client function to send/receive GET request data.
 	}
 
 	else {
-		Serial.println("Error on HTTP request");
+		Serial.println("Error on HTTP request");		
 	}
 	http.end();
+	return payload;
+}
+
+void parseForexJson(String payload) {
 	StaticJsonBuffer<1024> json_buf;
 	JsonObject &root = json_buf.parseObject(payload);
 	if (!root.success())
@@ -86,70 +88,19 @@ void getWeatherData() //client function to send/receive GET request data.
 	unsigned long timestamp = root["timestamp"];
 	Serial.println(value);
 	Serial.println(text);
-	Serial.println(timestamp);
 	time_t t = timestamp;
 	char buff[32];
 	sprintf(buff, "%02d.%02d.%02d %02d:%02d:%02d", day(t), month(t), year(t), hour(t), minute(t), second(t));
-	strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
 	Serial.println(buff);
-	//String result = "";
-	//const int httpPort = 80;
-	//if (!espClient.connect(servername2, httpPort)) {
-	//	return;
-	//}
-	//// We now create a URI for the request
-	//String url = "http://forex.1forge.com/1.0.3/convert?from=EUR&to=USD&quantity=1&api_key=C0PpsLfvczArzUfXOZdK7pkrskTmRP0q";
-
-	//// This will send the request to the server
-	//espClient.print(String("GET ") + url + " HTTP/1.1\r\n" +
-	//	"Host: " + servername2 + "\r\n" +
-	//	"Connection: close\r\n\r\n");
-	//unsigned long timeout = millis();
-	//while (espClient.available() == 0) {
-	//	if (millis() - timeout > 5000) {
-	//		espClient.stop();
-	//		return;
-	//	}
-	//}
-
-	//// Read all the lines of the reply from server
-	//while (espClient.available()) {
-	//	result = espClient.readStringUntil('\r');
-	//}
-
-	//result.replace('[', ' ');
-	//result.replace(']', ' ');
-
-	//char jsonArray[result.length() + 1];
-	//result.toCharArray(jsonArray, sizeof(jsonArray));
-	//jsonArray[result.length() + 1] = '\0';
-
-	//StaticJsonBuffer<1024> json_buf;
-	//JsonObject &root = json_buf.parseObject(jsonArray);
-	//if (!root.success())
-	//{
-	//	Serial.println("parseObject() failed");
-	//}
-
-	//String location = root["city"]["name"];
-	//String temperature = root["list"]["main"]["temp"];
-	//String weather = root["list"]["weather"]["main"];
-	//String description = root["list"]["weather"]["description"];
-	//String idString = root["list"]["weather"]["id"];
-	//String timeS = root["list"]["dt_txt"];
-
-	//weatherID = idString.toInt();
-	//Serial.print("WeatherID: ");
-	//Serial.println(weatherID);
-	//Serial.println(location);
-	//Serial.println(temperature);
-	//Serial.println(weather);
-	//Serial.println(description);
-	//Serial.println(timeS);
-	Serial.println(result);
 }
 
-void getCurrencyData() //client function to send/receive GET request data.
+void getForexData() //client function to send/receive GET request data.
+{
+	parseForexJson(getForexRate(usd_to_pln));
+	parseForexJson(getForexRate(eur_to_usd));
+}
+
+void getWeatherData() //client function to send/receive GET request data.
 {
 	String result = "";
 	const int httpPort = 80;
